c1214c151209fd6052635ab33feb297566f410e2
Graeme Fulton <graemefulton89@gmail.com>
2022-03-06 21:41:30 +0100

    remove password protect

--- pages/api/login.js
+++
@@ -1,6 +0,0 @@
-import { loginHandler } from "@storyofams/next-password-protect";
-
-export default loginHandler("protoverse", {
-  // Options go here (optional)
-  cookieName: "next-password-protect",
-});
--- pages/api/passwordCheck.js
+++
@@ -1,6 +0,0 @@
-import { passwordCheckHandler } from "@storyofams/next-password-protect";
-
-export default passwordCheckHandler("protoverse", {
-  // Options go here (optional)
-  cookieName: "next-password-protect",
-});
